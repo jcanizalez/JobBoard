f17335f43d14b3b88c5a32c381fbd70ef5667805
Javier Oswaldo Canizalez Flores <javier.canizalez@avianca.com>
2020-09-23 17:34:07 -0600

    Include swagger into API to better documentation on API

--- src/JobBoard/JobBoard.API/Startup.cs
+++ src/JobBoard/JobBoard.API/Startup.cs
@@ -26,11 +26,20 @@ namespace JobBoard.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jobs Boards API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
