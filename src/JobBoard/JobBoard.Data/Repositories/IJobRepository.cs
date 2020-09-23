93ae4f24126118412b8faf2002f4164f669bfbc4
Javier Oswaldo Canizalez Flores <javier.canizalez@avianca.com>
2020-09-23 17:47:05 -0600

    Define Interface for Job Repository

---
+++ src/JobBoard/JobBoard.Data/Repositories/IJobRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JobBoard.Model;
+
+namespace JobBoard.Data.Repositories
+{
+    public interface IJobRepository
+    {
+        Task<Job> GetJobByIdAsync(int? idJob);
+        Task<List<Job>> GetJobsAsync();
+        Task<Job> CreateJobAsync(Job job);
+        Task<bool> EditJobAsync(Job job);
+        Task<bool> DeleteJobAsync(Job job);
+
+    }
+}
---
+++ src/JobBoard/JobBoard.Data/Repositories/JobRepository.cs
@@ -0,0 +1,36 @@
+﻿using JobBoard.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoard.Data.Repositories
+{
+    public class JobRepository : IJobRepository
+    {
+        public Task<Job> CreateJobAsync(Job job)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteJobAsync(Job job)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditJobAsync(Job job)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Job> GetJobByIdAsync(int? idJob)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Job>> GetJobsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
