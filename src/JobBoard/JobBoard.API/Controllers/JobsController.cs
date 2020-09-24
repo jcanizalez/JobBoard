6b0622cb168a341a7dfd72f65965006dfe25943e
Javier Oswaldo Canizalez Flores <javier.canizalez@avianca.com>
2020-09-23 18:46:52 -0600

    Create Jobs Controller in the API

---
+++ src/JobBoard/JobBoard.API/Controllers/JobsController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JobBoard.Model;
+using JobBoard.Data.Repositories;
+
+namespace JobBoard.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+
+        private readonly IJobRepository _repository;
+
+        public JobsController(IJobRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/Jobs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Job>>> GetJobs()
+        {
+            return await _repository.GetJobsAsync();
+        }
+
+        // GET: api/Jobs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Job>> GetJob(int id)
+        {
+            var job = await _repository.GetJobByIdAsync(id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return job;
+        }
+
+
+        // POST: api/Jobs
+        [HttpPost]
+        public async Task<ActionResult<Job>> PostJob(Job job)
+        {
+            var resultJob = await _repository.CreateJobAsync(job);
+            return CreatedAtAction("GetJob", new { id = resultJob.Id }, job);
+        }
+
+
+        // PUT: api/TodoItems/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJob(int id, Job job)
+        {
+            if (id != job.Id)
+            {
+                return BadRequest();
+            }
+
+
+            var jobRead = await _repository.GetJobByIdAsync(id);
+            if (jobRead == null)
+            {
+                return NotFound();
+            }
+
+            await _repository.EditJobAsync(job);
+
+
+
+            return NoContent();
+        }
+
+        // DELETE: api/Jobs/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Job>> DeleteJob(int id)
+        {
+            var job = await _repository.GetJobByIdAsync(id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            await _repository.DeleteJobAsync(job);
+
+
+            return job;
+        }
+    }
+}
--- src/JobBoard/JobBoard.API/Startup.cs
+++ src/JobBoard/JobBoard.API/Startup.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using JobBoard.Data;
+using JobBoard.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +31,11 @@ namespace JobBoard.API
         {
             services.AddControllers();
             services.AddSwaggerGen();
+            services.AddScoped<IJobRepository, JobRepository>();
+            services.AddEntityFrameworkSqlite().AddDbContext<JobBoardContext>(options => options.UseSqlite("Filename=JobDatabase.db", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
